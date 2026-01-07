7c6756e4c17e8c10f68491db238f0794d83ae839
kang99999999 <imshinzle@naver.com>
2026-01-08 06:01:15 +0900

    최종

--- web/sanity.config.ts
+++ web/sanity.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'sanity'
 import { deskTool } from 'sanity/desk'
 import { visionTool } from '@sanity/vision'
 
-import { apiVersion, dataset, projectId } from './env'
-import { schemaTypes } from './schemaTypes'
+import { apiVersion, dataset, projectId } from './sanity/env'
+import { schemaTypes } from './sanity/schemaTypes'
 
 export default defineConfig({
   name: 'default',
