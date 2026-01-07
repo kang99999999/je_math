a7ce7cbc81905c2289fb81add2b0a485005da738
kang99999999 <imshinzle@naver.com>
2026-01-08 03:27:29 +0900

    sanity apiVersion수정

--- web/src/lib/sanity.ts
+++ web/src/lib/sanity.ts
@@ -1,15 +1,12 @@
+import { createClient } from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
-import {createClient} from 'next-sanity'
 
 export const sanityClient = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION!,
-  useCdn: true,  
+  apiVersion: '2023-10-01', // ⭐️ 반드시 하드코딩
+  useCdn: true,
 })
 
 const builder = imageUrlBuilder(sanityClient)
-
-export function urlFor(source: any) {
-  return builder.image(source)
-}
+export const urlFor = (source: any) => builder.image(source)
