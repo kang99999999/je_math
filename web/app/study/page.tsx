736613fc1180855f7667b2f2a73dc872b93926d0
kang99999999 <imshinzle@naver.com>
2026-01-16 15:08:23 +0900

    달력수정

--- web/app/study/page.tsx
+++ web/app/study/page.tsx
@@ -3,6 +3,10 @@ import { studyDatesQuery } from '@/lib/queries'
 import StudyPage from './StudyPage'
 
 export default async function StudyPageWrapper() {
-  const posts = await sanityClient.fetch(studyDatesQuery)
+    const posts = await sanityClient.fetch(
+    studyDatesQuery,
+    {},
+    { cache: 'no-store' } // ⭐ 이게 핵심
+  )
   return <StudyPage posts={posts} />
 }
